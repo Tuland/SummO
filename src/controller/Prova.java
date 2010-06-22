198de98fe36faf31688fa6ad1994f8a1cbf72f69
Tullio <tuland@gmail.com>
2010-06-22 21:06:27 +0200

    22_6 3-doc

--- src/controller/Prova.java
+++ src/controller/Prova.java
@@ -1,24 +1,16 @@
 /**
  * SummO: Ontology Summarization 
- * Copyright (C) 2010 Tullio Landoni (tuland@gmail.com)
+ * Copyright (C) 2010 Tullio Landoni
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/. 
  */
-package controller.printer;
-
-import controller.OntoBuilder;
-import model.OntoLoaded;
-
+package controller;
 
 /**
  * @author tuland
  *
  */
-public interface Printer {
-	/**
-	 * @param sourceOnto	a source ontology
-	 * @param builder		an ontology builder
-	 */
-	void print(OntoLoaded sourceOnto, OntoBuilder builder);
+public class Prova {
+
 }
--- src/controller/Starter.java
+++ src/controller/Starter.java
@@ -1,3 +1,10 @@
+/**
+ * SummO: Ontology Summarization 
+ * Copyright (C) 2010 Tullio Landoni (tuland@gmail.com)
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/. 
+ */
 package controller;
 
 import java.io.File;
@@ -30,6 +37,7 @@ public class Starter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		printLicense();
 		// Load the configuration file
 		try {
 			gConf = Yaml.loadType(new File(GENERAL_CONF_FILE), ConfBean.class);
@@ -47,6 +55,15 @@ public class Starter {
 		// Run summarization end print results
 		SummManager manager = new SummManager(summList);
 		manager.start(new EmbeddingWithPPPrinter()); 		
+	}
+	
+	private static void printLicense(){
+		System.out.println(
+			"SummO  Copyright (C) 2010  Tullio Landoni (tuland@gmail.com)\n" +
+			"This program comes with ABSOLUTELY NO WARRANTY.\n" +
+			"This is free software, and you are welcome to redistribute it " +
+			"under certain conditions.\n" + 
+			"Details: http://www.gnu.org/licenses/gpl.html \n\n");
 	}
 
 
